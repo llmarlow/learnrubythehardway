bbc2bc65adbf91f10c32d99bf30722f1a820bec3
Lorna Marlow <bikergirl1515@hotmail.com>
2015-10-01 13:57:05 +0000

    ex2-5

---
+++ ex5.rb
@@ -0,0 +1,29 @@
+name = 'Zed A. Shaw'
+age = 35 # not a lie in 2009
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall."
+puts "He's #{weight} pounds heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+# this line is tricky, try to get it exactly right
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
+
+
+#inches to centimeters
+inches = 3.46
+cm_in_inches = 2.5
+cm = inches * cm_in_inches
+puts "#{inches} inches = #{cm} cm."
+
+#pounds to kilograms
+pounds = 124
+lbs_in_kg = 2.2
+kg = pounds / lbs_in_kg
+puts "#{pounds} lbs = #{kg} kg."
